63bc2fa352ca151a1e3d7ef70b52bcb70b3903d6
bmochan <108970739+bmochan@users.noreply.github.com>
2022-11-14 19:11:07 +0600

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,3 +1,9 @@
+# put your token inside quotes
+TOKEN = ""
+
+# replace with server id's you want to autocatch on
+WHITELISTED_SERVERS = [69696969696, 420420420420]
+
 # library imports
 import discord
 import threading
@@ -5,30 +11,33 @@ import asyncio
 
 # module imports
 from src.utils import *
-from src.captcha_solver import solve
+from src.helpers import solve
 
 # load the model and initialize labels
 model = tf.keras.models.load_model("model_pokemon.h5")
 
-pokemons = # for self-use of labels, contact me on discord
+# contact me to get the values
+pokemons = []
 
 # kickstart the model to reduce prediction time
 identify(model, pokemons,
          "https://media.discordapp.net/attachments/1037799258637750363/1037814779005382716/pokemon.jpg")
 
-# client vars
-GUILD_ID = 6969696969 # replace with id of your server
-TOKEN = "" # put your token inside the quotation marks
 
 class Pokefier(discord.Client):
     async def on_ready(self):
+        self.verifying = False
         await self.change_presence(status=discord.Status.dnd)
         print('[READY] Logged in as', self.user)
 
     async def on_message(self, message):
-        if message.author.id == 716390085896962058:
+        if not hasattr(self, 'verifying'):
+            self.verifying = False
+
+        # only acknowledge the bot's messages if captcha is not being solved
+        if message.author.id == 716390085896962058 and self.verifying == False:
             if len(message.embeds) > 0 and "wild pokémon has appeared!" in message.embeds[0].title:
-                if message.guild.id == GUILD_ID:
+                if message.guild.id in WHITELISTED_SERVERS:
                     pokemon_image = message.embeds[0].image.url
 
                     await message.channel.trigger_typing()
@@ -36,9 +45,9 @@ class Pokefier(discord.Client):
                     threading.Thread(target=catch, args=(
                         self, model, pokemons, pokemon_image, message, asyncio.get_event_loop())).start()
 
-                    await asyncio.sleep(5)
-
             if message.content.startswith("Whoa there") and str(self.user.id) in message.content:
+                self.verifying = True
+
                 # getting the url from captcha message
                 url = message.content.split(
                     "Whoa there. Please tell us you're human! ")[1]
@@ -46,27 +55,11 @@ class Pokefier(discord.Client):
                 print("Got CAPTCHA! Attempting to solve.")
 
                 self.captcha_url = url
-                await self.close()
+
+                threading.Thread(target=solve, args=(
+                    client, self.captcha_url)).start()
 
 
-while True:
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-    try:
-        client = Pokefier(
-            guild_subscription_options=discord.GuildSubscriptionOptions.off())
-        loop.run_until_complete(client.start(TOKEN))
-
-    except:
-        pass
-
-    finally:
-        loop.close()
-        asyncio.set_event_loop(None)
-
-        # client has logged out
-        if hasattr(client, 'captcha_url'):
-            solve(client.captcha_url)
-
-            print("Successfully solved the CAPTCHA!")
+client = Pokefier(
+    guild_subscription_options=discord.GuildSubscriptionOptions.off())
+client.run(TOKEN)
